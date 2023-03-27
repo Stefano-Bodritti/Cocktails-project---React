2e3e070fa13c7cef76265ebf42e3db6cd6a995a1
Stefano <tepo@hotmail.it>
2023-03-27 21:39:35 +0200

    add fetching static data from API

--- src/context.js
+++ src/context.js
@@ -7,7 +7,40 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('a');
-  const [cocktails, setCoktails] = useState([]);
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const {drinks} = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {idDrink, strDrink, strDrinkThumb, strDrinkAlcoholic, strGlass} = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strDrinkAlcoholic,
+            glass: strGlass
+          };
+        })
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm])
 
   return (
     <AppContext.Provider
