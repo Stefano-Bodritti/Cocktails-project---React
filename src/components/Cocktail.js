bbceb79d6ebe996f50190316dcb138c1164b3863
Stefano <tepo@hotmail.it>
2023-03-27 21:45:41 +0200

    show list of cocktails

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cocktail = () => {
+const Cocktail = (props) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={props.image} alt={props.name} />
+      </div>
+      <div className="cocktails-footer">
+        <h3>{props.name}</h3>
+        <h4>{props.glass}</h4>
+        <p>{props.info}</p>
+        <Link to={`/cocktail/${props.id}`} className='btn btn-primary btn-details'>details</Link>
+      </div>
+    </article>
+  );
 }
 
 export default Cocktail
